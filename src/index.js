e2fefc1872ef27132d362e2eb9c4c4c8df013c3a
akshay968 <76764402+akshay968@users.noreply.github.com>
2022-06-30 13:48:01 +0530

    Added project files

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ getDoc ,updateDoc
 }from 'firebase/firestore';
 import {
   getAuth,createUserWithEmailAndPassword,signOut,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,onAuthStateChanged
 }from 'firebase/auth';
 
 const firebaseConfig = {
@@ -29,6 +29,7 @@ const firebaseConfig = {
 const db =getFirestore();
 const auth=getAuth();
 const eventref = collection(db,'Events');
+const profileref=collection(db,'profile');
 const create_event=document.querySelector('.createevent');
 if(create_event)
 {
@@ -42,7 +43,7 @@ if(create_event)
           Title_of_the_event:create_event.Title_of_the_event.value,
           Organising_by:create_event.Organising_by.value,
           Speakers:create_event.Speakers.value,
-         //EventDate:create_event.EventDate.value,
+         EventDate:create_event.Edate.value,
           Start_time:create_event.Start_time.value,
           End_time:create_event.End_time.value,
           Limit:create_event.Limit.value,
@@ -56,28 +57,29 @@ if(create_event)
 
 })
 }
+
  //signup for the users
     const signupboo = document.querySelector('.signup')
     const arri = []
    
     if(signupboo){
      
-      signupboo.addEventListener('click', (e)=>{
-        console.log("jhcvb")
+      signupboo.addEventListener('submit', (e)=>{
+        
     
     // function handlesignup(e){
       e.preventDefault();
       console.log(signupboo); 
-      const signup = document.querySelector('.signup')
-
-      const email= signup.suemail.value;
-      userName = email;
-      const password = signup.supassword.value
+      const signup1 = document.querySelector('.signup')
+console.log(signup1.email);
+      const email= signup1.email.value;
+      const userName = email;
+      const password = signup1.password.value
       const temppass = password;
       createUserWithEmailAndPassword(auth, email, password)
         .then((cred) =>{
             console.log('User Created', cred.user);
-            signup.reset()
+            signup1.reset()
         })
         .catch((err) => {
           console.log(err.message)
@@ -85,6 +87,87 @@ if(create_event)
       })
     }
       
+    const loginboo = document.querySelector('.login')
+    if(loginboo){
+      loginboo.addEventListener('click', handlelogin)
+    }
+    console.log("jhcvb")
+    
+    function handlelogin(e){
+      e.preventDefault()
+      const login = document.querySelector('.login')
+
+      const email = login.email.value
+      const userName = email;
+      const password = login.password.value
+      signInWithEmailAndPassword(auth, email, password)  
+      .then((cred)=>{
+        window.location.replace("../dist/index2.html");
+            console.log('user logged in', cred.user)
+        })
+        .catch((err)=>{
+          console.log(err.message)
+        })
+    }
+
+
+    //logout 
+    // const logout = document.querySelector('.logout')
+    // if(logout){
+    //   logout.addEventListener('click', ()=>{
+    //     signOut(auth)
+    //       .then(()=>{
+    //         // console.log('user signed out');
+    //         window.location.replace("../dist/homepage.html");
+    //       })
+    //       .catch((err)=>{
+    //         console.log(err.message);
+    //       })
+    //   })
+    // }
+    
+
+
+    //Subscribing to auth Changes
+    onAuthStateChanged(auth, (user) => {
+      console.log('user status changed', user);
+    })
+    const colRef1 = collection(db, 'Events')
+    const display = document.querySelector('.confii')
+    if(display){
+      display.addEventListener('click', (e)=>{
+        const appendindash = document.querySelector('.card-content4')
+        // append1.innerHTML = "Hiiiiii"
+        let divi ;
+        getDocs(colRef1)
+          .then((snapshot) => {
+              // console.log(snapshot.docs)
+              let books = []
+              snapshot.docs.forEach((doc) => {
+                  books.push({ ...doc.data(), id:doc.id})
+  
+                    divi = divi + `<div class="card col">
+                    <div class="card-img">
+                        <img src="Assets/3.jpg" alt="image">
+                    </div>
+                    <div class="card-text">
+                        <h2>${doc.data().Title_of_the_event}l</h2>
+                        <p>${doc.data().EventDate}</p>
+                        <div class="no-more"><button onclick="location.href='/dist/EventDescription.html';" type="button" class="btns">Know more</button></div>
+                    </div>
+                </div>`
+                  }
+                  
+              )
+              
+              appendindash.innerHTML = divi;
+              // console.log(books)
+          })
+          .catch(err => {
+              console.log(err.message)
+          })
+      })
+    }
 
 
 
@@ -92,8 +175,32 @@ if(create_event)
 
 
 
+let displayBtn = document.querySelector(".register_btn");
+displayBtn.addEventListener("click", (e) =>{
+  function myFunction() {
+    /* Get the text field */
+    let copy = document.querySelector(".copy");
+    copy.innerHTML = 'Copied';
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+    
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
 
-
+    
+  }
+  e.preventDefault();
+  let displayBox = document.querySelector(".display-link");
+  displayBox.innerHTML = `<input type="text" value="https://meet.google.com/wpz-giug-gmb" id="myInput">
+  <button class="btn-primary copy" onclick=${myFunction}>Copy text</button>`
+}
+);
 
 
 
